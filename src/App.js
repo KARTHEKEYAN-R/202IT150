a0f76b03f6f3ab7c46e6e5487eac3ecf0a5b8ba2
KARTHE07 <karthekeyan.it20@bitsathy.ac.in>
2023-07-26 11:24:29 +0530

    aa

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div><h1>Train Ticket Reservation</h1></div>
+      </header>
+      <div>
+        <div className='Sec_1'>
+          <h3>John Dae Railways</h3>
+          <div className='but'><button><a href='makemytrip.com'></a></button></div>
+        </div>
+        <div className='Sec_2'>
+          <h3>Norway Railways</h3><br></br>
+          <h4>Timings - 8:30AM</h4><br></br>
+          <h4>11:00AM</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
